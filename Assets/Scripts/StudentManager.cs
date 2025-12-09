b637e0f41dcc582d821fecbebe4034eda73da6aa
PrashanthAgithe <prashanthagithe@gmail.com>
2025-12-09 19:02:52 +0530

    Refactor scenes and settings for improved organization and functionality

--- Assets/Scripts/StudentManager.cs
+++ Assets/Scripts/StudentManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;   // NEW INPUT SYSTEM
 using System.Collections;
 
 public class StudentManager : MonoBehaviour
@@ -14,70 +15,54 @@ public class StudentManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            // Optionally DontDestroyOnLoad(gameObject);
         }
         else
         {
             Destroy(gameObject);
         }
     }
-    
+
     void Start()
     {
-        // find all students automatically
         students = FindObjectsByType<StudentController>(FindObjectsSortMode.None);
     }
 
     void Update()
     {
-        // Manual test: Press E to make a random student stand for 5 seconds
-        if (Input.GetKeyDown(KeyCode.E) && !isActionRunning)
+        // MANUAL TEST: Press E to make a random student stand for 5 seconds
+        if (!isActionRunning && Keyboard.current != null && 
+            Keyboard.current.eKey.wasPressedThisFrame)
         {
             StartCoroutine(RandomStudentStand());
         }
     }
 
-    /// <summary>
-    /// Manual random stand for 5 seconds (used for testing only)
-    /// </summary>
     IEnumerator RandomStudentStand()
     {
         isActionRunning = true;
 
-        // pick random student
         activeStudent = students[Random.Range(0, students.Length)];
 
-        // make him stand
         activeStudent.ToggleSitStand(false);
 
-        // wait 5 seconds
         yield return new WaitForSeconds(5f);
 
-        // make him sit again
         activeStudent.ToggleSitStand(true);
 
         isActionRunning = false;
     }
 
-    /// <summary>
-    /// Makes a random student stand during audio playback duration
-    /// Called from GeminiLLM
-    /// </summary>
     public IEnumerator StandForAudio(float duration)
     {
         if (isActionRunning) yield break;
         isActionRunning = true;
 
-        // pick random student
         activeStudent = students[Random.Range(0, students.Length)];
 
-        // stand before audio starts
         activeStudent.ToggleSitStand(false);
 
-        // wait for audio to finish
         yield return new WaitForSeconds(duration);
 
-        // sit back
         activeStudent.ToggleSitStand(true);
 
         isActionRunning = false;
